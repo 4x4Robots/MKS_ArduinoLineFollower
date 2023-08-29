e7df7e1353487319ff58db2632ee29130896ae2b
4x4Robots Moritz <119802108+4x4Robots@users.noreply.github.com>
2023-08-29 17:41:38 +0200

    Final Version

--- ArduinoFiles/LineFollower_3Sensors/LineFollower_3Sensors.ino
+++ ArduinoFiles/LineFollower_3Sensors/LineFollower_3Sensors.ino
@@ -14,7 +14,7 @@ const int PORT_motor_R = 2;   // Left Motor
 const int forward_backward_multiplier = -1;  // Direction of the Motors.
 
 const int C_black = 100;   // above is black
-const int speed_0 = 70;    // standard speed
+const int speed_0 = 95;
 
 /// Instances
 
@@ -27,26 +27,23 @@ int value_R;
 int value_L;
 int value_M;
 
-bool black_R;
-bool black_L;
-bool black_M;
-
 int speed_L;
 int speed_R;
 bool forward;
 
-bool S_LineWasLeft = false;
-bool S_LineWasRight = false;
+int time_straight;
+
+
 
 /// Functions
 
 void setSpeed(AF_DCMotor motor, int speed) {
   if(speed < -255) {
     speed = -255;
-    Serial.println("Error: setSpeed(): speed out of bounds");
+    //Serial.println("Error: setSpeed(): speed out of bounds");
   } else if(speed > 255) {
     speed = 255;
-    Serial.println("Error: setSpeed(): speed out of bounds");
+    //Serial.println("Error: setSpeed(): speed out of bounds");
   }
   speed = forward_backward_multiplier * speed;
 
@@ -61,10 +58,6 @@ void setSpeed(AF_DCMotor motor, int speed) {
   motor.setSpeed(abs(speed));
 }
 
-bool isBlack(int value) {
-  return value > C_black;
-}
-
 /// Setup
 
 void setup() {
@@ -75,12 +68,8 @@ void setup() {
   value_L = 0;
   value_M = 0;
 
-  black_R = false;
-  black_L = false;
-  black_M = false;
-
   speed_L = speed_0;
-  speed_R = speed_0
+  speed_R = speed_0;
   forward = true;
 
   setSpeed(motor_L, 0);
@@ -95,6 +84,7 @@ void loop() {
   value_R = analogRead(PIN_analogIn_R);
   value_M = analogRead(PIN_analogIn_M);
 
+
   Serial.print("\tL: ");
   Serial.print(value_L);
   Serial.print("\tR: ");
@@ -102,13 +92,37 @@ void loop() {
   Serial.print("\tM: ");
   Serial.println(value_M);
 
-  setSpeed(motor_L, speed_L);
-  setSpeed(motor_R, speed_R);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-
   Serial.print("Speed_L: ");
   Serial.print(speed_L);
   Serial.print("Speed_R: ");
   Serial.print(speed_R);
+
+  if(value_R > C_black && value_L < C_black){  //Rechtskurve
+    speed_L = speed_0;
+    speed_R = 0;
+    time_straight = 0;
+  }
+  else if(value_L > C_black && value_R < C_black){ //Linkskurve
+    speed_L = 0;
+    speed_R = speed_0;
+    time_straight = 0;
+  }
+  else if(value_M > C_black){ //Geradeaus
+
+    speed_L = speed_0;
+    speed_R = speed_0;
+    time_straight = time_straight + 1;
+  }
+  
+  if (time_straight > 50) {
+      time_straight = 0;
+      setSpeed(motor_L, speed_0);
+      setSpeed(motor_R, -speed_0);
+      delay(500);
+  }
+
+  setSpeed(motor_L,speed_L);
+  setSpeed(motor_R,speed_R);
 
   //delay(300);
 }

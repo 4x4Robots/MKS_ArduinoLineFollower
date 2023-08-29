47b1d937ff3f992ee23795339aab26d4377847e5
4x4Robots Moritz <119802108+4x4Robots@users.noreply.github.com>
2023-08-29 15:31:13 +0200

    Motor Test Function

--- ArduinoFiles/LineFollower_3Sensors/LineFollower_3Sensors.ino
+++ ArduinoFiles/LineFollower_3Sensors/LineFollower_3Sensors.ino
@@ -13,8 +13,8 @@ const int PORT_motor_R = 2;   // Left Motor
 
 /// Instances
 
-AF_DCMotor motorL(PORT_motor_L);
-AF_DCMotor motorR(PORT_motor_R);
+AF_DCMotor motor_L(PORT_motor_L);
+AF_DCMotor motor_R(PORT_motor_R);
 
 /// Global Variables
 
@@ -22,17 +22,51 @@ int value_R;
 int value_L;
 int value_M;
 
+int speed;
+bool forward;
+
+
+
+/// Functions
+
+void setSpeed(AF_DCMotor motor, int speed) {
+  if(speed < -255) {
+    speed = -255;
+    Serial.println("Error: setSpeed(): speed out of bounds");
+  } else if(speed > 255) {
+    speed = 255;
+    Serial.println("Error: setSpeed(): speed out of bounds");
+  }
+
+  if(speed == 0) {
+    motor.run(RELEASE);
+  } else if(speed < 0) {
+    motor.run(BACKWARD);
+  } else if(speed > 0) {
+    motor.run(FORWARD);
+  }
+
+  motor.setSpeed(abs(speed));
+}
+
+/// Setup
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
   value_R = 0;
-  vlaue_L = 0;
+  value_L = 0;
   value_M = 0;
+
+  speed = 0;
+  forward = true;
+
+  setSpeed(motor_L, 0);
+  setSpeed(motor_R, 0);
 }
 
-
+/// Loop
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -40,17 +74,33 @@ void loop() {
   value_R = analogRead(PIN_analogIn_R);
   value_M = analogRead(PIN_analogIn_M);
 
-  Serial.print("L: ");
+  Serial.print("\tL: ");
   Serial.print(value_L);
   Serial.print("\tR: ");
   Serial.print(value_R);
   Serial.print("\tM: ");
   Serial.println(value_M);
 
-  delay(30);
+  if(forward) {
+    if(speed < 255) {
+      setSpeed(motor_L, speed);
+      setSpeed(motor_R, speed);
+      speed = speed + 10;
+    } else {
+      forward = false;
+    }
+  } else {
+    if(speed > -255) {
+      setSpeed(motor_L, speed);
+      setSpeed(motor_R, speed);
+      speed = speed - 10;
+    } else {
+      forward = true;
+    }
+  }
+
+  Serial.print("Speed: ");
+  Serial.print(speed);
+
+  delay(300);
 }
-
-
-
-}
-

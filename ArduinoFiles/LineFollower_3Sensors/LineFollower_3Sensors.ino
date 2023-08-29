b01d1b5410d5549557b001ffa503e924ab1f22f7
4x4Robots Moritz <119802108+4x4Robots@users.noreply.github.com>
2023-08-29 15:02:18 +0200

    Declare Constants for Sensors and Motors

--- ArduinoFiles/LineFollower_3Sensors/LineFollower_3Sensors.ino
+++ ArduinoFiles/LineFollower_3Sensors/LineFollower_3Sensors.ino
@@ -1,3 +1,22 @@
+/// Includes
+
+#include <AFMotor.h>
+
+/// Constants
+
+const int PIN_analogIn_L = A0;  // Left Sensor
+const int PIN_analogIn_R = A2;  // Right Sensor
+const int PIN_analogIn_M = A1;  // Left Sensor
+
+const int PORT_motor_L = 1;   // Right Motor
+const int PORT_motor_R = 2;   // Left Motor
+
+/// Instances
+AF_DCMotor motorL(PORT_motor_L);
+AF_DCMotor motorR(PORT_motor_R);
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
